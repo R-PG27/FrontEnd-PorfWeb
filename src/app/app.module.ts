e106d9705f48b27a328dfe862196fb554ec74766
R-PG27 <rodrigo.gormaz@mi.unc.edu.ar>
2023-01-07 12:59:42 -0300

    FrontEnd.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EducationComponent } from './components/education/education.component';
+import { HSSkillsComponent } from './components/hsskills/hsskills.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    BannerComponent,
+    AboutMeComponent,
+    EducationComponent,
+    HSSkillsComponent,
+    ProjectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
