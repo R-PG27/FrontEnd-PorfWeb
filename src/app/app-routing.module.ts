a48c8a84816d6746c6460c21ebf87ad71c13e5c6
R-PG27 <rodrigo.gormaz@mi.unc.edu.ar>
2023-01-17 21:33:12 -0300

    FrontEnd.9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewEducationComponent } from './components/education/new-education.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path: 'newedu', component: NewEducationComponent}
 ];
 
 @NgModule({
